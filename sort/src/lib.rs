f5c810aa6c117586fe7af0b25d5aeb83b307f81e
Matrosov Sergey <matrosik14@gmail.com>
2020-10-14 01:30:47 +0300

    remove dummy tests

--- sort/src/lib.rs
+++ sort/src/lib.rs
@@ -3,15 +3,3 @@ mod heap_sort;
 
 pub use selection_sort::selection_sort;
 pub use heap_sort::heap_sort;
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-
-}
