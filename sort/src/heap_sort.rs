79d2e935338dcc1217515606d1ad66ddf9517971
Matrosov Sergey <matrosik14@gmail.com>
2020-10-21 01:45:05 +0300

    start heap_sort

--- sort/src/heap_sort.rs
+++ sort/src/heap_sort.rs
@@ -1,5 +1,26 @@
-pub fn heap_sort(_vec: &mut [i64]) {
+fn sift(heap: &mut [i64]) {
+    let mut i: usize = 0;
+    let mut j: usize = 0;
+    let x = heap[0];
+    if j < heap.len() && heap[j+1] < heap[j] { j += 1; }
+    while j < heap.len() && heap[j] < x {
+        heap[i] = heap[j];
+        i = j;
+        j = 2 * j;
+        if j < heap.len() && heap[j+1] < heap[j] { j += 1; }
+    }
 
+}
+
+pub fn heap_sort(vec: &mut [i64]) {
+    let mut right_idx: usize = vec.len();
+    while right_idx > 0 {
+        let x = vec[0];
+        vec[0] = vec[right_idx];
+        vec[right_idx] = x;
+        right_idx -= 1;
+        sift(&mut vec[..right_idx]);
+    }
 }
 
 #[cfg(test)]
