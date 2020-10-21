05f8d4a9a7f89967e800fa73014d2f38d1c04bdc
Matrosov Sergey <matrosik14@gmail.com>
2020-10-22 02:06:36 +0300

    save curr progress on heap_sort

--- sort/src/heap_sort.rs
+++ sort/src/heap_sort.rs
@@ -1,23 +1,35 @@
 fn sift(heap: &mut [i64]) {
-    let mut i: usize = 0;
-    let mut j: usize = 0;
-    let x = heap[0];
-    if j < heap.len() && heap[j+1] < heap[j] { j += 1; }
-    while j < heap.len() && heap[j] < x {
-        heap[i] = heap[j];
-        i = j;
-        j = 2 * j;
-        if j < heap.len() && heap[j+1] < heap[j] { j += 1; }
-    }
+    let mut root: usize = 0;
+    loop {
+        let mut child = 2 * root + 1;
+        if child >= heap.len() { break; }
+        if child + 1 < heap.len() && heap[child] < heap[child + 1] {
+            child += 1;
+        }
 
+        if heap[root] < heap[child] {
+            heap.swap(root, child);
+            root = child;
+        } else {
+            break;
+        }
+    }
+    // let mut i: usize = 0;
+    // let mut j: usize = 0;
+    // let x = heap[0];
+    // if j < heap.len() && heap[j+1] < heap[j] { j += 1; }
+    // while j < heap.len() && heap[j] < x {
+    //     heap.swap(i, j);
+    //     i = j;
+    //     j = 2 * j;
+    //     if j < heap.len() && heap[j+1] < heap[j] { j += 1; }
+    // }
 }
 
 pub fn heap_sort(vec: &mut [i64]) {
-    let mut right_idx: usize = vec.len();
+    let mut right_idx: usize = vec.len() - 1;
     while right_idx > 0 {
-        let x = vec[0];
-        vec[0] = vec[right_idx];
-        vec[right_idx] = x;
+        vec.swap(0, right_idx);
         right_idx -= 1;
         sift(&mut vec[..right_idx]);
     }
