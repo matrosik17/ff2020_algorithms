80f89438ce235eec17a3653e7a68777fa38bf23a
Matrosov Sergey <matrosik14@gmail.com>
2020-10-23 12:44:44 +0300

    update tests for selection_sort

--- sort/src/selection_sort.rs
+++ sort/src/selection_sort.rs
@@ -1,4 +1,4 @@
-pub fn selection_sort(vec: &mut [i64]) {
+pub fn selection_sort<T: Ord>(vec: &mut [T]) {
     for i in 0..vec.len() {
         let mut small = i;
         for j in (i + 1)..vec.len() {
@@ -13,14 +13,31 @@ pub fn selection_sort(vec: &mut [i64]) {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::prelude::*;
 
     #[test]
     fn select_sort_test() {
-        let mut test_vec = vec![5, 3, 2, 4, 1];
-        let expected_vec = vec![1, 2, 3, 4, 5];
+        let mut test_vec = vec![5, 1, 2, 4, 1];
+        let expected_vec = vec![1, 1, 2, 4, 5];
 
         selection_sort(&mut test_vec);
         assert_eq!(test_vec, expected_vec);
+    }
+
+    #[test]
+    fn select_sort_rand_test() {
+        let n_samples = 100;
+        let arr_len = 1000;
+        let expected_vec: Vec<i64> = (0i64..arr_len).map(i64::from).collect();
+        let mut rng = thread_rng();
+
+        for _ in 0..n_samples {
+            let mut test_vec = expected_vec.clone();
+            test_vec.shuffle(&mut rng);
+
+            selection_sort(&mut test_vec);
+            assert_eq!(test_vec, expected_vec);
+        }
     }
 
 }
