4155c5496227fd18e94c96f6da39ce5326b42e39
Matrosov Sergey <matrosik14@gmail.com>
2020-11-26 00:00:11 +0300

    update bench file naming

--- sort/src/bench_utils/mod.rs
+++ sort/src/bench_utils/mod.rs
@@ -1,11 +1,11 @@
 mod common;
 mod generate_collection;
 mod count_wrapper;
-mod compare_time;
-mod compare_ord;
+mod compare_time_seq;
+mod compare_time_ord;
 
 pub use common::*;
 pub use generate_collection::{generate_seq, generate_ord_collection};
 pub use count_wrapper::Counter;
-pub use compare_time::*;
-pub use compare_ord::*;
+pub use compare_time_seq::*;
+pub use compare_time_ord::*;
